813bf2eb395ecb7053777c87eb425a6e41d38ea7
mriganka.paul <paul.mriganka@gmail.com>
2020-12-06 20:39:57 -0500

    First Module completed

--- my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
+++ my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import (aws_s3 as _s3, core)
+from aws_cdk import (aws_s3 as _s3, aws_iam as _iam, core)
 
 
 class MyFirstCdkProjectStack(core.Stack):
@@ -12,6 +12,22 @@ class MyFirstCdkProjectStack(core.Stack):
             self,
             "myBucketId",
             bucket_name = "myfirstcdkproject-mp",
-            versioned=True,
-            encryption=_s3.BucketEncryption.KMS_MANAGED
+            versioned=False,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        myBucket = _s3.Bucket(
+            self,
+            "myBucketId1"
+        )
+
+        _iam.Group(self, "gid")
+
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value = myBucket.bucket_name,
+            description=f"My First CDK bucket",
+            export_name="myBucketOutput1"
         )

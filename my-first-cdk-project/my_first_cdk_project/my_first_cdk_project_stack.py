565b8d29acfe5da4ffd64678a4e1605efdd2d114
mriganka.paul <paul.mriganka@gmail.com>
2020-12-06 22:09:50 -0500

    Created s3 buckets for prod and dev profile

--- my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
+++ my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,33 +1,23 @@
-from aws_cdk import (aws_s3 as _s3, aws_iam as _iam, core)
+from aws_cdk import (aws_s3 as _s3, core)
 
 
-class MyFirstCdkProjectStack(core.Stack):
+class MyArtifactBucketStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, is_prod=False, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
 
-        _s3.Bucket(
-            self,
-            "myBucketId",
-            bucket_name = "myfirstcdkproject-mp",
-            versioned=False,
-            encryption=_s3.BucketEncryption.S3_MANAGED,
-            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
-        )
-
-        myBucket = _s3.Bucket(
-            self,
-            "myBucketId1"
-        )
-
-        _iam.Group(self, "gid")
-
-        output_1 = core.CfnOutput(
-            self,
-            "myBucketOutput1",
-            value = myBucket.bucket_name,
-            description=f"My First CDK bucket",
-            export_name="myBucketOutput1"
-        )
+        if is_prod:
+            artifactBucket = _s3.Bucket(
+                self, 
+                "myProdArtifactBucketId", 
+                versioned=True, 
+                encryption=_s3.BucketEncryption.S3_MANAGED, 
+                removal_policy=core.RemovalPolicy.RETAIN
+                )
+        else: 
+            artifactBucket = _s3.Bucket(
+                self,
+                "myDevArtifactBucketId"
+            )

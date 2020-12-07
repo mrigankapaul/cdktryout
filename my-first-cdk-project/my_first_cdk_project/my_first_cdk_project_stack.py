f6a81410cef518f41d381d40e77ce95be6e05741
mriganka.paul <paul.mriganka@gmail.com>
2020-12-07 08:56:29 -0500

    Parameterized KMS ecryption for s3 bucket

--- my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
+++ my-first-cdk-project/my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import (aws_s3 as _s3, core)
+from aws_cdk import (aws_s3 as _s3, aws_kms as _kms, core)
 
 
 class MyArtifactBucketStack(core.Stack):
@@ -8,15 +8,27 @@ class MyArtifactBucketStack(core.Stack):
 
         # The code that defines your stack goes here
 
-        # to refer from cdk.json
-        print(self.node.try_get_context('us')['account'])
+        # print(self.node.try_get_context('default')['kms_arn'])
 
+        # to refer from cdk.json
+
+        
+
+        # if is_prod:
+        #     artifactBucket = _s3.Bucket(
+        #         self, 
+        #         "myProdArtifactBucketId", 
+        #         versioned=True, 
+        #         encryption=_s3.BucketEncryption.KMS,
+        #          encryption_key = _kms.Key.from_key_arn(self, "myKeyId", self.node.try_get_context('default')['kms_arn']),
+        #         removal_policy=core.RemovalPolicy.RETAIN
+        #         )
         if is_prod:
             artifactBucket = _s3.Bucket(
                 self, 
                 "myProdArtifactBucketId", 
                 versioned=True, 
-                encryption=_s3.BucketEncryption.S3_MANAGED, 
+                encryption=_s3.BucketEncryption.S3_MANAGED,
                 removal_policy=core.RemovalPolicy.RETAIN
                 )
         else: 
